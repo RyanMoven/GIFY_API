11672ce3713c33bd870ad78964dd53fa26e4b41f
RyanMoven <ryanmoven@gmail.com>
2017-12-05 14:59:48 -0500

    Organized code a bit

--- giphy.js
+++ giphy.js
@@ -1,33 +1,49 @@
 $(document ).ready(function(){
 
-	$("#find-food").on("click", function(event) {
+	var foodList = [];
 
-        // event.preventDefault() can be used to prevent an event's default behavior.
-        // Here, it prevents the submit button from trying to submit a form when clicked
-        event.preventDefault();
 
-        // Here we grab the user's response from the input box
-        var userFood = $("#food-input").val();
-
-        							// had to include user's response in search
+function foodDisplay() {
+ var userFood = $(this).data("search");	
+ 					// had to include user's response in search
 $.ajax({
       url: "https://api.giphy.com/v1/gifs/search?q=" + userFood + "&api_key=Hoj2gICqMvbl7IYI8j8nNdHz8Or5bbde&limit=10&rating=g",
       method: "GET"
     }).done(function(response) {
       console.log(response);
+      var foodData = response.data;
+
+      for (var i = 0; i < foodData.length; i++) {
+      	// created divs for each loop
+      	var foodDiv = $("<div>");
+      	//creating an image
+      	var foodImage = $("<img>");
+
+      	//assigning the attr & class for each image
+      	foodImage.addClass("foodItem");
+      	foodImage.attr("data-animate")
+      	//appending gif to div
+      	foodDiv.append(foodImage);
+      	//appending gif to food-view
+      	$("#food-view").append(foodDiv);
+
+      }
+
+
+
+
 
     	}); // end of ajax call
+} // end of foodDisplay
 
 
 
-	}); // end of onclick function
+	 
 
 
 }); //end of document ready
 
 
   
-
-
 
 	
